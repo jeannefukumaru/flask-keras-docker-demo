ed11720fced88cbac64121558ea16cd909f6cabf
Jeanne Choo <jchoo@aisingapore.org>
2018-07-06 15:46:52 +0800

    included some sample images

--- run_keras_server.py
+++ run_keras_server.py
@@ -5,6 +5,7 @@ from PIL import Image
 import numpy as np
 import flask
 import io
+import tensorflow as tf
 
 # initialise Flask application and Keras model 
 app = flask.Flask(__name__)
@@ -17,6 +18,8 @@ def load_model():
     '''
     global model
     model = ResNet50(weights='imagenet')
+    global graph
+    graph = tf.get_default_graph()
 
 def prepare_image(image, target):
     # convert image if not RGB
@@ -49,21 +52,22 @@ def predict():
 
             ## classify the input image and then initialise the list
             # of predictions to return to the client 
-            preds = model.predict(image)
-            results = imagenet_utils.decode_predictions(preds)
-            data['predictions'] = []
+            with graph.as_default():
+                preds = model.predict(image)
+                results = imagenet_utils.decode_predictions(preds)
+                data['predictions'] = []
 
-            # loop over the results and add them to the list of returned
-            # predictions
-            for (imageNetID, label, prob) in results[0]:
-                r = {'label': label, 'probability': float(prob)}
-                data['predictions'].append(r)
+                # loop over the results and add them to the list of returned
+                # predictions
+                for (imageNetID, label, prob) in results[0]:
+                    r = {'label': label, 'probability': float(prob)}
+                    data['predictions'].append(r)
 
-            # indicate that the request was a success 
-            data['success'] = True
+                # indicate that the request was a success 
+                data['success'] = True
     return flask.jsonify(data)
             
 if __name__=="__main__":
     print(('* loading Keras model and Flask starting server'))
     load_model()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
